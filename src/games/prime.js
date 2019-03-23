404e4cf31eeb633a27f375d51112c19ae508f0fa
Konstantin <sithari41441@gmail.com>
2019-03-23 21:44:25 +0700

    Реализована игра Простое ли число?

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,30 @@
 // brain-prime game logic
 
-/* import playGame from '..';
+import playGame from '..';
 import getRandomNumber from '../utils';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
 
-const isSimple = (number) => {
+const isPrime = (number) => {
+  let result = false; // Ибо линтер "ожидает увидеть return в конце функции"
 
+  if (number === 1) {
+    result = false;
+  }
+
+  for (let divider = 2; divider * divider <= number; divider += 1) {
+    if (number % divider === 0) {
+      return false;
+    }
+    result = true;
+  }
+
+  return result;
 };
 
 const generateGameData = () => {
   const number = getRandomNumber(1, 100);
-  const correctAnswer = String(isSimple(number));
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   const questionText = `${number}`;
 
   return { questionText, correctAnswer };
@@ -19,4 +32,4 @@ const generateGameData = () => {
 
 const playProgression = () => playGame(gameDescription, generateGameData);
 
-export default playProgression; */
+export default playProgression;
